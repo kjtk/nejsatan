3923300b8b67e47d38af344d9ae8543aa7eafb26
kjtk <kari.knaapi@gmail.com>
2020-01-09 15:35:23 +0200

    Liikkumisen testausta.

---
+++ Assets/KariTests/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 input;
+    public float speed = 1;
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * new Vector3(input.axis.x, 0, input.axis.y);
+    }
+}
