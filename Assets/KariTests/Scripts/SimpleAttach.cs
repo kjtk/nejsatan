f84bf0820b1ec1dff92a92d49e785f7e4a6863da
kjtk <kari.knaapi@gmail.com>
2020-01-10 11:28:47 +0200

    Grab-testit

---
+++ Assets/KariTests/Scripts/SimpleAttach.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour {
+
+    Interactable interactable;
+
+    void Start() {
+        interactable = GetComponent<Interactable>();
+    }
+
+    void OnHandHoverBegin(Hand hand) {
+        hand.ShowGrabHint();
+    }
+
+    void OnHandHoverEnd(Hand hand) {
+        hand.HideGrabHint();
+    }
+
+    void OnHandHoverUpdate(Hand hand) {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        // Grab
+        if (interactable.attachedToHand == null && grabType != GrabTypes.None) {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+        }
+        // Release
+        else if(isGrabEnding) {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+
+}
