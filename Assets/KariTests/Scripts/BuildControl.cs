e1fcf34e9a9fe609f50d0a47657b13bce325da29
kjtk <kari.knaapi@gmail.com>
2020-01-13 16:04:40 +0200

    Collision + simple snap.

--- Assets/KariTests/Scripts/BuildControl.cs
+++ Assets/KariTests/Scripts/BuildControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BuildControl : MonoBehaviour
 {
+    public List<GameObject> PartList = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
